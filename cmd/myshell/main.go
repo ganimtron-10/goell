8648296b463be5eabb811c691e930031da3ae538
Praneeth Shetty <praneethshetty10@gmail.com>
2024-12-26 12:16:02 +0530

    looping infinitely

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,16 +10,19 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
 
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+		// Wait for user input
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
 
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
