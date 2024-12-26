30a283951a8babd87dec81c8a8580783aec3c07e
Praneeth Shetty <praneethshetty10@gmail.com>
2024-12-26 12:32:58 +0530

    implement echo

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,14 +8,22 @@ import (
 	"strings"
 )
 
-func evalExit(input []string) {
+func exit(input []string) {
 	exitCode, err := strconv.Atoi(input[1])
 
 	if err != nil {
 		fmt.Println("Unable to parse Exit Code. Error Details: " + err.Error())
+		os.Exit(1)
 	}
 
 	os.Exit(exitCode)
+}
+
+func echo(input []string) {
+	for _, ele := range input[1:] {
+		fmt.Print(ele, " ")
+	}
+	fmt.Println()
 }
 
 func evalCommand(command string) {
@@ -25,7 +33,9 @@ func evalCommand(command string) {
 	splittedCommand := strings.Split(command, " ")
 	switch splittedCommand[0] {
 	case "exit":
-		evalExit(splittedCommand)
+		exit(splittedCommand)
+	case "echo":
+		echo(splittedCommand)
 	default:
 		fmt.Println(command + ": command not found")
 	}
