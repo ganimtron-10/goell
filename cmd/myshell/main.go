00789be55ea269f67efce3afd1d389b7eed79851
Praneeth Shetty <praneethshetty10@gmail.com>
2024-12-27 12:07:04 +0530

    adding tidla support for cd

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -93,6 +93,10 @@ func cd(args []string) {
 		return
 	}
 
+	if args[0] == "~" {
+		args[0] = os.Getenv("HOME")
+	}
+
 	err := os.Chdir(args[0])
 	if err != nil {
 		fmt.Printf("cd: %s: No such file or directory\n", args[0])
