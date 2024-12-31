479b1104c72f347524b9258fe2870281f5438663
Praneeth Shetty <praneethshetty10@gmail.com>
2024-12-31 13:18:52 +0530

    adding redirection

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -19,21 +20,83 @@ const (
 )
 
 func exit(args []string) {
+	args, _, errorWriter := checkRedirection(args)
 	exitCode, err := strconv.Atoi(args[0])
 
 	if err != nil {
-		fmt.Println("Unable to parse Exit Code. Error Details: " + err.Error())
+		fmt.Fprintln(errorWriter, "Unable to parse Exit Code. Error Details: "+err.Error())
 		os.Exit(1)
 	}
 
 	os.Exit(exitCode)
 }
 
-func echo(args []string) {
-	for _, ele := range args {
-		fmt.Print(ele, " ")
+func checkRedirection(args []string) ([]string, io.Writer, io.Writer) {
+
+	normalWriter, errorWriter := os.Stdout, os.Stderr
+	hasRedirection := false
+
+	for i := len(args) - 1; i > 0; i-- {
+		switch args[i] {
+		case ">", "1>":
+			hasRedirection = true
+			file, err := os.OpenFile(args[i+1], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+
+			if err != nil {
+				fmt.Fprintln(errorWriter, "Error while handling redirection file. Error details: "+err.Error())
+				os.Exit(1)
+			}
+
+			// fmt.Println("Normal Redirection")
+			normalWriter = file
+		case "2>":
+			hasRedirection = true
+			file, err := os.OpenFile(args[i+1], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+
+			if err != nil {
+				fmt.Fprintln(errorWriter, "Error while handling redirection file. Error details: "+err.Error())
+				os.Exit(1)
+			}
+
+			// fmt.Println("Error Redirection")
+			errorWriter = file
+		case ">>", "1>>":
+			hasRedirection = true
+			file, err := os.OpenFile(args[i+1], os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+			if err != nil {
+				fmt.Fprintln(errorWriter, "Error while handling redirection file. Error details: "+err.Error())
+				os.Exit(1)
+			}
+
+			// fmt.Println("Append Normal Redirection")
+			normalWriter = file
+		case "2>>":
+			hasRedirection = true
+			file, err := os.OpenFile(args[i+1], os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+			if err != nil {
+				fmt.Fprintln(errorWriter, "Error while handling redirection file. Error details: "+err.Error())
+				os.Exit(1)
+			}
+
+			// fmt.Println("append Error Redirection")
+			errorWriter = file
+		}
 	}
-	fmt.Println()
+
+	if hasRedirection {
+		args = args[:len(args)-2]
+	}
+	return args, normalWriter, errorWriter
+}
+
+func echo(args []string) {
+	args, normalWriter, _ := checkRedirection(args)
+	for _, ele := range args {
+		fmt.Fprint(normalWriter, ele, " ")
+	}
+	fmt.Fprintln(normalWriter)
 }
 
 func checkExecutable(commandName string) string {
@@ -56,6 +119,7 @@ func checkExecutable(commandName string) string {
 }
 
 func evalType(args []string) {
+	args, normalWriter, errorWriter := checkRedirection(args)
 	commandList := []string{EXIT, ECHO, TYPE, PWD, CD}
 	isInbuilt := false
 
@@ -71,24 +135,26 @@ func evalType(args []string) {
 	executablePath := checkExecutable(args[0])
 
 	if isInbuilt {
-		fmt.Println(args[0] + " is a shell builtin")
+		fmt.Fprintln(normalWriter, args[0]+" is a shell builtin")
 	} else if executablePath != "" {
-		fmt.Println(args[0] + " is " + executablePath)
+		fmt.Fprintln(normalWriter, args[0]+" is "+executablePath)
 	} else {
-		fmt.Println(args[0] + ": not found")
+		fmt.Fprintln(errorWriter, args[0]+": not found")
 	}
 }
 
-func pwd() {
+func pwd(args []string) {
+	args, normalWriter, errorWriter := checkRedirection(args)
 	dirPath, err := os.Getwd()
 	if err != nil {
-		fmt.Println("Error while getting current working dirtectory. Error details: " + err.Error())
+		fmt.Fprintln(errorWriter, "Error while getting current working dirtectory. Error details: "+err.Error())
 		os.Exit(1)
 	}
-	fmt.Println(dirPath)
+	fmt.Fprintln(normalWriter, dirPath)
 }
 
 func cd(args []string) {
+	args, _, errorWriter := checkRedirection(args)
 	if len(args) < 1 {
 		return
 	}
@@ -99,107 +165,108 @@ func cd(args []string) {
 
 	err := os.Chdir(args[0])
 	if err != nil {
-		fmt.Printf("cd: %s: No such file or directory\n", args[0])
+		fmt.Fprintf(errorWriter, "cd: %s: No such file or directory\n", args[0])
 	}
 }
 
 func execute(executablePath string, args []string) {
+	args, _, errorWriter := checkRedirection(args)
 	command := exec.Command(executablePath, args...)
 	command.Stderr = os.Stderr
 	command.Stdout = os.Stdout
 	err := command.Run()
 	if err != nil {
-		fmt.Println("Error while executing " + executablePath + ". Error details: " + err.Error())
+		fmt.Fprintln(errorWriter, "Error while executing "+executablePath+". Error details: "+err.Error())
 		os.Exit(1)
 	}
 }
 
-func modifiedFields(str string) []string {
-	modFields := []string{}
-	orgFields := strings.Fields(str)
-	for _, ele := range orgFields {
-		modFields = append(modFields, strings.ReplaceAll(ele, "\\", ""))
-	}
-	return modFields
-}
+// func modifiedFields(str string) []string {
+// 	modFields := []string{}
+// 	orgFields := strings.Fields(str)
+// 	for _, ele := range orgFields {
+// 		modFields = append(modFields, strings.ReplaceAll(ele, "\\", ""))
+// 	}
+// 	return modFields
+// }
 
-func parseCommand(command string) []string {
-	parsedCommand := []string{}
+// func parseCommand(command string) []string {
+// 	parsedCommand := []string{}
 
-	firstSpaceLocation := strings.Index(command, " ")
-	if firstSpaceLocation != -1 {
-		parsedCommand = append(parsedCommand, command[:firstSpaceLocation])
-	}
-	command = command[firstSpaceLocation+1:]
+// 	firstSpaceLocation := strings.Index(command, " ")
+// 	if firstSpaceLocation != -1 {
+// 		parsedCommand = append(parsedCommand, command[:firstSpaceLocation])
+// 	}
+// 	command = command[firstSpaceLocation+1:]
 
-	for {
-		start := strings.IndexAny(command, "'\"")
-		if start == -1 {
-			parsedCommand = append(parsedCommand, modifiedFields(command)...)
-			break
-		}
-		parsedCommand = append(parsedCommand, modifiedFields(command[:start])...)
+// 	for {
+// 		start := strings.IndexAny(command, "'\"")
+// 		if start == -1 {
+// 			parsedCommand = append(parsedCommand, modifiedFields(command)...)
+// 			break
+// 		}
+// 		parsedCommand = append(parsedCommand, modifiedFields(command[:start])...)
 
-		quote := command[start]
-		command = command[start+1:]
-		end := strings.IndexByte(command, quote)
-		parsedCommand = append(parsedCommand, command[:end])
-		command = command[end+1:]
-	}
+// 		quote := command[start]
+// 		command = command[start+1:]
+// 		end := strings.IndexByte(command, quote)
+// 		parsedCommand = append(parsedCommand, command[:end])
+// 		command = command[end+1:]
+// 	}
 
-	return parsedCommand
-}
+// 	return parsedCommand
+// }
 
-func parseCommand2(command string) []string {
-	parsedCommand := []string{}
-	isSingleQuote := false
-	isDoubleQuote := false
-	// isEscaped := false
-	curToken := ""
+// func parseCommand2(command string) []string {
+// 	parsedCommand := []string{}
+// 	isSingleQuote := false
+// 	isDoubleQuote := false
+// 	// isEscaped := false
+// 	curToken := ""
 
-	for i := 0; i < len(command); i++ {
-		char := string(command[i])
+// 	for i := 0; i < len(command); i++ {
+// 		char := string(command[i])
 
-		if char == "'" {
-			if isDoubleQuote {
-				curToken += char
-			}
-			isSingleQuote = !isSingleQuote
-		} else if char == "\"" {
-			if isDoubleQuote {
-				isDoubleQuote = false
-			} else {
-				if isSingleQuote {
-					curToken += char
-				}
-				isDoubleQuote = true
-			}
-		} else if char == "\\" {
-			if isSingleQuote {
-				curToken += string(command[i])
-			} else {
-				if i+1 < len(command) {
-					i++
-					curToken += string(command[i])
-				}
-			}
-		} else if char == " " && !isSingleQuote && !isDoubleQuote {
-			if curToken != "" {
-				parsedCommand = append(parsedCommand, curToken)
-				curToken = ""
-			}
-		} else {
-			curToken += char
-		}
+// 		if char == "'" {
+// 			if isDoubleQuote {
+// 				curToken += char
+// 			}
+// 			isSingleQuote = !isSingleQuote
+// 		} else if char == "\"" {
+// 			if isDoubleQuote {
+// 				isDoubleQuote = false
+// 			} else {
+// 				if isSingleQuote {
+// 					curToken += char
+// 				}
+// 				isDoubleQuote = true
+// 			}
+// 		} else if char == "\\" {
+// 			if isSingleQuote {
+// 				curToken += string(command[i])
+// 			} else {
+// 				if i+1 < len(command) {
+// 					i++
+// 					curToken += string(command[i])
+// 				}
+// 			}
+// 		} else if char == " " && !isSingleQuote && !isDoubleQuote {
+// 			if curToken != "" {
+// 				parsedCommand = append(parsedCommand, curToken)
+// 				curToken = ""
+// 			}
+// 		} else {
+// 			curToken += char
+// 		}
 
-	}
-	if curToken != "" {
-		parsedCommand = append(parsedCommand, curToken)
-	}
+// 	}
+// 	if curToken != "" {
+// 		parsedCommand = append(parsedCommand, curToken)
+// 	}
 
-	// fmt.Println(parsedCommand)
-	return parsedCommand
-}
+// 	// fmt.Println(parsedCommand)
+// 	return parsedCommand
+// }
 
 func parseCommand3(command string) []string {
 	parsedCommand := []string{}
@@ -269,14 +336,15 @@ func evalCommand(command string) {
 	case TYPE:
 		evalType(splittedCommand[1:])
 	case PWD:
-		pwd()
+		pwd(splittedCommand[1:])
 	case CD:
 		cd(splittedCommand[1:])
 	default:
 		if executablePath := checkExecutable(splittedCommand[0]); executablePath != "" {
 			execute(executablePath, splittedCommand[1:])
 		} else {
-			fmt.Println(command + ": command not found")
+			_, _, errorWriter := checkRedirection(splittedCommand[1:])
+			fmt.Fprintln(errorWriter, command+": command not found")
 		}
 
 	}
